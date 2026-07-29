271f95076173d274fac1064000ab7a6a4cca2a7e
macbook <callie@heycallie.net>
2026-07-29 01:21:13 -0400

    fixed some stuff and begun index

--- main.js
+++ main.js
@@ -18,26 +18,26 @@ document
 
 function changeBackgroundWaterlilies() {
   const element = document.querySelector(".fullDocument");
-  element.style.backgroundImage = "url('assets/windows_xp_102.jpg')";
+  element.style.backgroundImage = "url('assets/images/windows_xp_102.jpg')";
 }
 
 function changeBackgroundDefault() {
   const element = document.querySelector(".fullDocument");
-  element.style.backgroundImage = "url('assets/windows_xp_14.jpg')";
+  element.style.backgroundImage = "url('assets/images/windows_xp_14.jpg')";
 }
 
 function changeBackgroundTulips() {
   const element = document.querySelector(".fullDocument");
-  element.style.backgroundImage = "url('assets/windows_xp_35.jpg')";
+  element.style.backgroundImage = "url('assets/images/windows_xp_35.jpg')";
 }
 
 function regStartMenuPress() {
   smb = document.getElementById("startMenuButton");
   if (!document.getElementById("startMenu").classList.contains("hidden")) {
-    smb.style.backgroundImage = "url('assets/start_hover_v2.png')";
+    smb.style.backgroundImage = "url('assets/images/start_hover_v2.png')";
     sm.classList.toggle("hidden");
   } else {
-    smb.style.backgroundImage = "url('assets/start_pressed_v2.png')";
+    smb.style.backgroundImage = "url('assets/images/start_pressed_v2.png')";
     sm = document.getElementById("startMenu");
     sm.classList.toggle("hidden");
   }
@@ -48,7 +48,7 @@ function regStartMenuLeave() {
   if (!document.getElementById("startMenu").classList.contains("hidden")) {
     return;
   }
-  smb.style.backgroundImage = "url('assets/start_normal_v2.png')";
+  smb.style.backgroundImage = "url('assets/images/start_normal_v2.png')";
 }
 
 function regStartMenuEnter() {
@@ -56,7 +56,7 @@ function regStartMenuEnter() {
   if (!document.getElementById("startMenu").classList.contains("hidden")) {
     return;
   }
-  smb.style.backgroundImage = "url('assets/start_hover_v2.png')";
+  smb.style.backgroundImage = "url('assets/images/start_hover_v2.png')";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
